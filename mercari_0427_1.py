5bf2a2c79e7ed448e55794337862ea395fdf1de7
Tomoaki Sugiayma <sti320a@gmail.com>
2019-04-27 14:47:59 +0900

    Update

--- mercari_0427_1.py
+++ mercari_0427_1.py
@@ -1,3 +1,24 @@
 import pandas as pd
 train = pd.read_csv(f'./dataset/train.tsv', sep='\t')
 test = pd.read_csv(f'./dataset/test.tsv', sep='\t')
+
+print('=== train.shape ===')
+print(train.shape)
+
+print('=== test.shape ===')
+print(test.shape)
+
+print('=== dtypes ===')
+print(train.dtypes)
+
+print('=== train.head ===')
+print(train.head())
+
+print('=== train.price.describe() ===')
+print(train.price.describe())
+
+print('=== train.shipping.describe() ===')
+print(train.shipping.describe())
+
+print('=== train.shipping.mean() ===')
+print(train.shipping.mean())
